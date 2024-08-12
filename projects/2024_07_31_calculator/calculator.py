f5c1e003a5271634fb210b5622cb25291e423b58
Karol <karol.kulak20@gmail.com>
2024-08-12 15:36:07 +0200

    init

---
+++ projects/2024_07_31_calculator/calculator.py
@@ -0,0 +1,45 @@
+class Calculator:
+    def __init__(self):
+        self.menu_string = '''
+        Menu:
+        1. Dodaj
+        2. Odejmij
+        3. Pomnóż
+        4. Podziel
+        5. Wyjdź'''
+
+    def print_menu(self):
+        print(self.menu_string)
+
+    def calculate(self, option, number1, number2):
+        match option:
+            case 1:
+                return number1 + number2
+            case 2:
+                return number1 - number2
+            case 3:
+                return number1 * number2
+            case 4:
+                if number2 == 0:
+                    print("Nie dzielimy przez 0")
+                else:
+                    return number1 / number2
+
+    def run(self):
+        option = 0
+        while option != 5:
+            self.print_menu()
+            option = int(input("Wybierz opcję:"))
+            if option == 5:
+                break
+            else:
+                number1 = int(input("Podaj Liczbę 1:"))
+                number2 = int(input("Podaj Liczbę 2:"))
+                result = self.calculate(option, number1, number2)
+                if result is not None:
+                    print(f"Twój wynik to: {result}")
+        print("Koniec programu")
+
+
+cal = Calculator()
+cal.run()
---
+++ todo.md
@@ -0,0 +1,2 @@
+- [ ] learn whole markdown syntax ".md"
+- ogarnąć githuba + break na kalk git init na data_science_course - repo taka sama nazwa
